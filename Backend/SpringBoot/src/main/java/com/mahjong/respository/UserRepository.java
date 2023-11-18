b505ff35bf7e7a52e7231f9ec209a9b220d63ba4
Depressedon9MUA <g46zhou@uwaterloo.ca>
2023-11-18 17:04:30 -0500

    init

--- Backend/SpringBoot/src/main/java/com/mahjong/respository/UserRepository.java
+++ Backend/SpringBoot/src/main/java/com/mahjong/respository/UserRepository.java
@@ -3,6 +3,7 @@ package com.mahjong.springboot;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     @Query("select users from ")
